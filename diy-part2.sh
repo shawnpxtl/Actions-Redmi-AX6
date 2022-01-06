aeacdb1320d08eeb41724daebde95c95b73e6398
ChrisBlue <67815438+chrisblue@users.noreply.github.com>
2022-01-07 00:31:46 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,6 +11,7 @@
 #
 
 # Modify default IP
+sed -i 's/10.10.10.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 # 修改连接数
