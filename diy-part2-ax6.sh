74bef4b7613135174dbdd6301318935555f325b1
ChrisBlue <67815438+chrisblue@users.noreply.github.com>
2022-01-07 00:41:24 +0800

    diy-part2.sh --> diy-part2-ax6.sh

--- diy-part2-ax6.sh
+++ diy-part2-ax6.sh
@@ -10,11 +10,9 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-# Modify default IP and password(Redmi AX6)
+# Modify default IP and password
 sed -i 's/10.10.10.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 sed -i 's/root:$1$WplwC1t5$HBAtVXABp7XbvVjG4193B.:18753:0:99999:7:::/root:$1$Fll0rJpx$ofJM67fckgmHNdiBPLdXu1:18998:0:99999:7:::/g' package/base-files/files/etc/shadow
-# Modify default IP (Phicomm K2P)
-sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 # 修改连接数
 #sed -i 's/net.netfilter.nf_conntrack_max=.*/net.netfilter.nf_conntrack_max=65535/g' package/kernel/linux/files/sysctl-nf-conntrack.conf
