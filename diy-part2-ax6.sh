c17ce85137713bc7c96aa9470e4fe82b220cc9bc
Michaellan <67815438+chrisblue@users.noreply.github.com>
2022-03-09 18:05:11 +0800

    edit ip address and password

--- diy-part2-ax6.sh
+++ diy-part2-ax6.sh
@@ -11,7 +11,8 @@
 #
 
 # Modify default IP and password
-sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+sed -i 's/10.10.10.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+sed -i 's/root:$1$WplwC1t5$HBAtVXABp7XbvVjG4193B.:18753:0:99999:7:::/root:$1$x1Rl0w.z$O0rVKf0HUu6qK23Znj0R8.:19060:0:99999:7:::/g' package/base-files/files/etc/shadow
 
 # 修改连接数
 #sed -i 's/net.netfilter.nf_conntrack_max=.*/net.netfilter.nf_conntrack_max=65535/g' package/kernel/linux/files/sysctl-nf-conntrack.conf
