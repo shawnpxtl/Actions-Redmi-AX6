827f478c9a8ab16bfb8fa03e2e3a07d9d7446d60
Shawn. TianLing <67815438+shawnpxtl@users.noreply.github.com>
2023-02-02 20:01:37 +0800

    Update diy-part2-ax6.sh

--- diy-part2-ax6.sh
+++ diy-part2-ax6.sh
@@ -23,6 +23,7 @@ sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=65535' package
 # git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/luci-theme-opentomcat
 # git clone https://github.com/openwrt-develop/luci-theme-atmaterial.git package/luci-theme-atmaterial
 git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
 
 # git clone https://github.com/sirpdboy/luci-app-netdata.git package/luci-app-netdata
 
