5267332e17b53be932caac2850fbbf163a798c1b
Shawn. TianLing <67815438+shawnpxtl@users.noreply.github.com>
2023-03-26 15:30:06 +0800

    add adguardhome

--- diy-part2-ax6.sh
+++ diy-part2-ax6.sh
@@ -24,16 +24,14 @@ sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=65535' package
 # git clone https://github.com/openwrt-develop/luci-theme-atmaterial.git package/luci-theme-atmaterial
 git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 
-# git clone https://github.com/sirpdboy/luci-app-netdata.git package/luci-app-netdata
-
 # 添加额外软件包
-# git clone https://github.com/panther706/luci-app-adguardhome package/luci-app-adguardhome
-# git clone https://github.com/sbwml/luci-app-adguardhome.git package/luci-app-adguardhome
+# git clone https://github.com/sirpdboy/luci-app-netdata.git package/luci-app-netdata
 git clone https://github.com/vernesong/OpenClash.git package/OpenClash
+git clone https://github.com/xiaoxiao29/luci-app-adguardhome.git package/luci-app-adguardhome
 
 # 添加核心温度的显示
-sed -i 's|pcdata(boardinfo.system or "?")|luci.sys.exec("uname -m") or "?"|g' feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
-sed -i 's/or "1"%>/or "1"%> ( <%=luci.sys.exec("expr `cat \/sys\/class\/thermal\/thermal_zone0\/temp` \/ 1000") or "?"%> \&#8451; ) /g' feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
+# sed -i 's|pcdata(boardinfo.system or "?")|luci.sys.exec("uname -m") or "?"|g' feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
+# sed -i 's/or "1"%>/or "1"%> ( <%=luci.sys.exec("expr `cat \/sys\/class\/thermal\/thermal_zone0\/temp` \/ 1000") or "?"%> \&#8451; ) /g' feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
 
 # Modify NTP Server
 sed -i "s/0.openwrt.pool.ntp.org/ntp.aliyun.com/g" package/base-files/files/bin/config_generate
